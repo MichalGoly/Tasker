32954bf706ca3ab087ba8962cc7f5869199c64d0
Will Jones <willjones1996@live.co.uk>
2016-01-29 12:33:13 +0000

    Comments and code formatting for this webpage

--- src/taskerMAN/date.php
+++ src/taskerMAN/date.php
@@ -1,3 +1,4 @@
+<!--file to create dates/set dates to variables then compares them. To help with datepicker validation -->
 <?php
 $date1=date_create("2013-03-15");
 $date2=date_create("2013-12-12");
