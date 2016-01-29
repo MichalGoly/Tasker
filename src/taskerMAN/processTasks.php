ed3239db8b81fc6be8c9fcb536bb600524fae6df
Will Jones <willjones1996@live.co.uk>
2016-01-29 13:40:31 +0000

    Comments and code formatting for this webpage

--- src/taskerMAN/processTasks.php
+++ src/taskerMAN/processTasks.php
@@ -19,11 +19,13 @@ header("Location: login.php");
   <meta content="IE=edge" http-equiv="X-UA-Compatible">
   <meta content="width=device-width, initial-scale=1" name="viewport">
   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  
   <meta content="" name="description">
   <meta content="" name="author">
   <link href="../../favicon.ico" rel="icon">
   <title>TaskerMAN</title>
   <!-- Bootstrap core CSS -->
+  
   <link href="css/bootstrap.min.css" rel="stylesheet">
   
   <!-- Custom styles for this template -->
@@ -46,37 +48,42 @@ header("Location: login.php");
         <!-- navbar CSS -->
           <button aria-controls="navbar" aria-expanded="false" class=
           "navbar-toggle collapsed" data-target="#navbar" data-toggle=
-          "collapse" type="button"><span class="sr-only">Toggle
-          navigation</span> <span class="icon-bar"></span> <span class=
+          "collapse" type="button"><span class="sr-only">
+          	
+          Toggle navigation</span> <span class="icon-bar"></span> <span class=
           "icon-bar"></span> <span class="icon-bar"></span></button> <a class=
           "navbar-brand" href="http://users.aber.ac.uk/tig/taskerMAN">TaskerMAN</a>
         </div>
+        
         <div class="navbar-collapse collapse" id="navbar">
           <ul class="nav navbar-nav navbar-right">
 		  <li><a href="#" ><?php echo "Logged in as ". $username;?></a></li>
             <li>
-               <a href="http://users.aber.ac.uk/tig/taskerMAN/logout.php"><i class="fa fa-sign-out" style="font-size:24px;"></i></a>
+               <a href="http://users.aber.ac.uk/tig/taskerMAN/logout.php">
+               	<!-- font awesome button -->
+               	<i class="fa fa-sign-out" style="font-size:24px;"></i></a>
             </li>
           </ul>
         </div>
       </div>
     </nav>
     
+    <!-- centres the jumbotron --> 
     <div class="containerCentre">
     <div class="container">
       <br>
       <br>
       <br>
+      
       <!-- CSS div classes --> 
 	  <h1 class="page-header"></h1>
       <div class="jumbotron">
         <ol class="breadcrumb">
           <li><a href="index.php"><i class="fa fa-chevron-circle-left" style="font-size:24px;"></i></a></li>
+          </ol>
           
           <!-- Inserts data from add task webpage into DB -->
-          
-        </ol>
-		<?php
+          <?php
 		$eviltitle = $_POST['title'];
 		$evilemail = $_POST['mEmail'];
 		
@@ -90,19 +97,18 @@ header("Location: login.php");
 		
 		?>
 		<?php
-		
-		$sDate = strip_tags($_POST['sDate']);
+		$sDate = strip_tags($_POST['sDate']); //strips html tags from input
 		$eDate = strip_tags($_POST['eDate']);
 		$title = strip_tags($_POST['title']);
 		$email = strip_tags($_POST['mEmail']);
-		//echo strrchr("Hello world!","!");
 		
 		if (strpos($title, '*') !== false || strpos($email, '*') !== false  ){
 			
 			session_destroy();
 			header('Location: login.php');	
 		}else{
-		
+					//function to stop users entering an end-date 
+					//that is beore the start date
 					$intlast1 = substr($sDate,8,2);
 					$intlast2 = substr($eDate,8,2);
 
@@ -117,30 +123,15 @@ header("Location: login.php");
 			
 					echo "<div class='alert alert-danger' align='center'>.";
 					echo $first4sec."-".$mid2."-".$intlast2;
+		    
+		    //Error message that should be thrown when the user tries
+		    //to enter an end date before the start date
                     echo "<strong> <BR><BR>DATE ERROR - COMPLETION DATE CANNOT BE BEFORE START DATE";
                     echo "<BR><BR><BR><a href='AddTaskBoot.php'><font color = 'red'>TRY AGAIN</color></a>";
                     echo "</strong>";
                     echo "</div>";
 		}else{
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-                  
-                  include 'connector.php';
-
-
-                
-                   
-
-
-
+		include 'connector.php'; //connects to the DB
                 $required = array('title','sDate','eDate','mEmail');
                 $error = false;
                 foreach($required as $field) {
@@ -154,29 +145,22 @@ header("Location: login.php");
                 } else {
                     $setStatusTo2 = 1; //allocated
                   //echo $setStatusTo1 ;
-
-                            
                 }   
-
-
-
+                //inserts all the fields into the Task table
                 $task = "INSERT INTO Task (title, startDate, endDate, taskStatus,TeamMember_email)
                         VALUES ('$title','$sDate','$eDate','$setStatusTo2','$email'
                         )";
 
-
                 if($connection->query($task)===TRUE){
                     
-                        
-                        
                         $array = $_POST['tDescription'];
-
                             foreach($array as $value){
                                     $element = "INSERT INTO TaskElement (description, Task_taskId)
                                                 VALUES ('$value',(SELECT taskId FROM Task WHERE title = '$title'))";
                                     
                             
                                 if($connection->query($element)===TRUE){
+                                    // confiration message
                                     echo "<div class='alert alert-success'>.";
                                     echo "<strong><img src='img/AddTask.png'> <BR><BR>SUCCESSFULLY ADDED ";
                                     echo "<BR><BR><BR><a href='AddTaskBoot.php'>ADD ANOTHER TASK</a>";
@@ -185,6 +169,7 @@ header("Location: login.php");
                                 }
                             }
                 }else{
+                	//error message
                         echo "<div class='alert alert-danger'>.";
                         echo " <strong><img src='img/warninguser.png'> <BR><BR>ERROR - Incorrect Syntax in Add Task field"; //for actual error message use this--> $connection->error.";
                         echo "<BR><BR><BR><a href='AddTaskBoot.php'>TRY AGAIN</a>";
