1cc3e2a37f0cb95477f8fa6c73526975022503e7
Will Jones <willjones1996@live.co.uk>
2016-01-29 13:28:48 +0000

    Comments and code formatting for this webpage

--- src/taskerMAN/logout.php
+++ src/taskerMAN/logout.php
@@ -1,5 +1,5 @@
-<?PHP
-
+<!-- a PHP file to log users out, it just destroys the session and then re-directs the user to the login page --> 
+<?php
 session_start();
 session_destroy();
 header("Location: login.php");
