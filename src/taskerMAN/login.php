7b2dac52ff1944a733bd0501d0666b0a068acf5f
Will Jones <willjones1996@live.co.uk>
2016-01-29 13:27:09 +0000

    Comments and code formatting for this webpage

--- src/taskerMAN/login.php
+++ src/taskerMAN/login.php
@@ -1,21 +1,14 @@
-	
+	<!-- A file to handle log in of users --> 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-
-
-
 
  define('DB_SERVER', 'db.dcs.aber.ac.uk:3306');
 define('DB_USERNAME', 'csgpadm_12');
 define('DB_PASSWORD', 'RCORoND6');
 define('DB_DATABASE', 'csgp_12_15_16');
+
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
-
-
-
-   
-  
 if(empty($_POST["username"]) || empty($_POST["password"])){
 
 		$error = "Both fields are required.";
@@ -74,6 +67,7 @@ $password=$_POST['password'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
@@ -83,36 +77,19 @@ $password=$_POST['password'];
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
     <!-- Custom styles for this template -->
     <link href="jumbotron.css" rel="stylesheet">
 
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
-
-   
-   
-
-   
-	
      <div class="container">
-	
 	 <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar" ng-bind="bar">
-          </ul>
+	 	<!-- formats the line under the nav bar -->
+           <div class="col-sm-3 col-md-2 sidebar">
+             <ul class="nav nav-sidebar" ng-bind="bar">
+              </ul>
           <ul class="nav nav-sidebar" ng-init="list=['Home','Users','Tasks','Logout']" ng-model="current">
            
           </ul>
@@ -124,10 +101,11 @@ $password=$_POST['password'];
 		  <BR>
 		  <BR>
 		  <BR>	
-		<label class="control-label col-sm-9" for="fname"></label>
+	<label class="control-label col-sm-9" for="fname"></label>
       <div class="col-sm-9">  
+      
 	<div class = "panel panel-primary">
-   <div class = "panel-heading">
+   	<div class = "panel-heading">
       <h3 class = "panel-title" align="center">TaskerMAN Login</h3>
 	
    </div>
@@ -146,11 +124,9 @@ $password=$_POST['password'];
       </div>
     </div>
 
-	
-	
-	
     			<div class="form-group">
      				 <div class="col-sm-5">
+     				 	<!-- input field for username --> 
         				<input type="text" class="form-control"  name="username"  placeholder="Username" required>
 	
      				 </div>
@@ -159,6 +135,7 @@ $password=$_POST['password'];
 	 
 			<div class="form-group">
       				<div class="col-sm-5">
+      					<!-- input field for password -->
         				<input type="password" class="form-control" name="password" placeholder="Password" required>
 	
       				</div>
@@ -166,6 +143,7 @@ $password=$_POST['password'];
 	
 	
 	 		<div class="form-group">
+	 			<!-- submit button --> 
       				<div class="col-sm-4">
 	          			<input  type="submit" value="Authenticate" class="btn btn-primary">
 					</div>
@@ -179,16 +157,13 @@ $password=$_POST['password'];
       <footer>
         <p>&copy; 2015 TaskerMan, Inc.</p>
       </footer>
-    <!-- /container -->
 
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
+
+   
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="../../dist/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
+  </body> 
 </html>
